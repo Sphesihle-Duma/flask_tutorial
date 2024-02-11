6e720c3118c2667cbe53bfdeb1460ec15e512beb
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-02-11 21:21:03 +0200

    database migration

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 '''main microblog app
 '''
-from mainapp import app
+from mainapp import app, db
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from mainapp.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    ''' flask shell context
+    '''
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
 
 
 if __name__ == '__main__':
